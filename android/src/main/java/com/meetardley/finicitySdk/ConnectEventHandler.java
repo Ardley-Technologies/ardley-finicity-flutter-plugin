c2254f489746a9a39f54e2c2166891634b4ccc8f
nathandenherder <nate@ardley.co>
2020-09-20 08:15:14 -0400

    updating package name

---
+++ android/src/main/java/com/meetardley/finicitySdk/ConnectEventHandler.java
@@ -0,0 +1,21 @@
+package com.meetardley.finicitySdk;
+
+import com.finicity.connect.sdk.EventHandler;
+import org.json.JSONObject;
+
+public class ConnectEventHandler implements EventHandler{
+
+    public void onLoaded(){
+
+    }
+    public void onDone(JSONObject doneEvent) {
+
+    }
+    public void onCancel() {
+
+    }
+    public void onError(JSONObject errorEvent){}
+    public void onRouteEvent(JSONObject routeEvent){}
+    public void onUserEvent(JSONObject userEvent){}
+
+}
--- android/src/main/java/com/meetardley/finicitySdk/FlutterSdkPlugin.java
+++ android/src/main/java/com/meetardley/finicitySdk/FlutterSdkPlugin.java
@@ -1,28 +1,54 @@
-package com.example.flutter_sdk;
+package com.meetardley.finicitySdk;
 
+import android.app.Activity;
 import androidx.annotation.NonNull;
 
 import com.finicity.connect.sdk.Connect;
-
+import android.content.Context;
+import com.meetardley.finicitySdk.ConnectEventHandler;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 
 /** FlutterSdkPlugin */
-public class FlutterSdkPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterSdkPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private Context context;
+  private Activity activity;
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+
+  }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_sdk");
     channel.setMethodCallHandler(this);
+    context = flutterPluginBinding.getApplicationContext();
   }
 
   @Override
@@ -34,8 +60,11 @@ public class FlutterSdkPlugin implements FlutterPlugin, MethodCallHandler {
       case "finicityConnectLite":
         String url = call.argument("url");
         if (url != null) {
-          Connect foo = new Connect();
+          ConnectEventHandler x = new ConnectEventHandler();
+          Connect.start( activity, url, new ConnectEventHandler());
+
         }
+        result.success(null);
         break;
       default:
         result.notImplemented();
