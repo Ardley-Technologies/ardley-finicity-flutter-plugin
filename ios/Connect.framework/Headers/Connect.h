d4fc44898836a643749a772aee44e377ce6b53f6
nathandenherder <nate@ardley.co>
2020-09-23 10:25:45 -0400

    updating framework

---
+++ ios/Connect.framework/Headers/Connect.h
@@ -0,0 +1,18 @@
+//
+//  Connect.h
+//  Connect
+//
+//  Copyright © 2020 finicity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Connect.
+FOUNDATION_EXPORT double ConnectVersionNumber;
+
+//! Project version string for Connect.
+FOUNDATION_EXPORT const unsigned char ConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Connect/PublicHeader.h>
+
+
